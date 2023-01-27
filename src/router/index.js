589bb0603ab84af8702892b625fe006a01267445
Elijahitiary <Elijah.itiary@icloud.com>
2023-01-27 18:17:44 +0200

    add 404 error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LessonsList from '../view/LessonsList.vue'
 import ShoppingList from '../view/ShoppingList.vue'
+import NotFoundPage from '../view/NotFoundPage.vue'
+
 const routes = [
-    {
-        path: '/',
-        name: 'LessonsList',
-        component: LessonsList
-    },
-    {
-        path: '/Shopping',
-        name: 'ShoppingList',
-        component: ShoppingList,
-    }
+  {
+    path: '/',
+    name: 'LessonsList',
+    component: LessonsList,
+  },
+  {
+    path: '/Shopping',
+    name: 'ShoppingList',
+    component: ShoppingList,
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: NotFoundPage,
+  },
 ]
 
-
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 })
 
 export default router
---
+++ src/view/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>404: Page Not Found</h1>
+  <img src="../assets/404.png" alt="" />
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage',
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+img {
+  width: 30vw;
+}
+</style>
